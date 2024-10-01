6c14ad8b7cd852ce5d4e87aac72642a6926e3777
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-01 21:55:39 +0600

    modified bounce method

--- Python Pong/ball.py
+++ Python Pong/ball.py
@@ -8,7 +8,16 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
         self.goto(position)
+        self.x_move = 10
+        self.y_move=10
+
     def move_ball(self):
-        new_x = self.xcor()+10
-        new_y = self.ycor() + 10
+        new_x = self.xcor()+self.x_move
+        new_y = self.ycor() +self.y_move
         self.goto(new_x,new_y)
+
+    def bounce_y(self):
+        self.y_move*=-1
+
+    def bounce_x(self):
+        self.x_move*=-1
