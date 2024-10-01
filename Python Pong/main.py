bbf82aa9fbd3b18677d723eecd22db9c46287dd0
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-01 23:41:00 +0600

    modified and add new functionality

--- Python Pong/main.py
+++ Python Pong/main.py
@@ -1,8 +1,10 @@
+from time import sleep
 from turtle import  Turtle , Screen
 from draw import Draw
 from paddle import Paddle
 from ball import  Ball
 import time
+from score import Score
 screen = Screen()
 screen.setup(800,600)
 screen.bgcolor("black")
@@ -10,7 +12,6 @@ screen.title("The Famous Pong Arcade Game")
 screen.tracer(0)
 draw =Draw()
 draw.draw_line()
-draw.draw_box()
 r_paddle=Paddle((370,0))
 l_paddle = Paddle((-370,0))
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,9 +20,11 @@ screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down,"s")
 screen.listen()
 ball = Ball((0,0))
+score = Score()
+
 game_on = True
-while game_on:
-    time.sleep(0.1)
+while game_on :
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move_ball()
     #Detect collision with wall
@@ -31,7 +34,10 @@ while game_on:
     if ball.distance(r_paddle)<50 and ball.xcor()>340 or ball.distance(l_paddle)<50 and ball.xcor()<-340:
         ball.bounce_x()
     #see if paddle miss the ball
-    if ball.xcor()>350 or ball.xcor()<-350:
-        ball.goto(0,0)
-        ball.bounce_x()
+    if ball.xcor()>350:
+        ball.reset_position()
+        score.plus_left()
+    if ball.xcor() < -350:
+        ball.reset_position()
+        score.plus_right()
 screen.exitonclick()
