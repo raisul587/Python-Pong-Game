a3d112d6c29c6ff639a3c6bfcd611869a843f6ec
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-01 23:38:26 +0600

    added ball moving speed

--- Python Pong/ball.py
+++ Python Pong/ball.py
@@ -10,6 +10,7 @@ class Ball(Turtle):
         self.goto(position)
         self.x_move = 10
         self.y_move=10
+        self.move_speed = 0.1
 
     def move_ball(self):
         new_x = self.xcor()+self.x_move
@@ -21,3 +22,9 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move*=-1
+        self.move_speed*=0.7
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
