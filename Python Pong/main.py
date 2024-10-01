a4e86f69abd8c94c25cbb429fd9319c74ab0f705
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-01 12:52:28 +0600

    added paddle class

--- Python Pong/main.py
+++ Python Pong/main.py
@@ -1,11 +1,22 @@
 from turtle import  Turtle , Screen
 from draw import Draw
+from paddle import Paddle
 screen = Screen()
-screen.setup(1080,600)
+screen.setup(800,600)
 screen.bgcolor("black")
+screen.title("The Famous Pong Arcade Game")
 screen.tracer(0)
 draw =Draw()
 draw.draw_line()
 draw.draw_box()
-screen.update()
+r_paddle=Paddle((350,0))
+l_paddle = Paddle((-350,0))
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down,"s")
+screen.listen()
+game_on = True
+while game_on:
+    screen.update()
 screen.exitonclick()
