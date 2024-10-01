0654908921fdfeabad7bd6c7295c7eede134aff6
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-01 23:38:53 +0600

    remove unwanted elements

--- Python Pong/draw.py
+++ Python Pong/draw.py
@@ -16,17 +16,18 @@ class Draw(Turtle):
             self.forward(10)
             self.penup()
             self.forward(10)
-    def draw_box(self):
-        self.pensize(10)
-        self.goto(-100,200)
-        self.pendown()
-        for _ in range(4):
-            self.forward(20)
-            self.right(90)
-            self.forward(70)
-        self.penup()
-        self.goto(100,270)
-        self.pendown()
-        self.pensize(10)
-        self.forward(70)
-        
+    # Testing
+    # def draw_box(self):
+    #     self.pensize(10)
+    #     self.goto(-100,200)
+    #     self.pendown()
+    #     for _ in range(4):
+    #         self.forward(20)
+    #         self.right(90)
+    #         self.forward(70)
+    #     self.penup()
+    #     self.goto(100,270)
+    #     self.pendown()
+    #     self.pensize(10)
+    #     self.forward(70)
+    #
